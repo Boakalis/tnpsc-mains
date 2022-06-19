7b62ec7f36d4ebbea69d3c6158ee001df6469ae3
BoopathyDrag <Boopathy.kr@codetez.com>
2022-06-19 18:39:46 +0530

    Added Nr

--- resources/js/src/Home.js
+++ resources/js/src/Home.js
@@ -9,72 +9,74 @@ import BreadCrumbs from "./components/BreadCrumbs";
 import axios from "./helpers/axios";
 import Skeleton from "@mui/material/Skeleton";
 import "./Exams/Exam.css";
-
+import TextLoader from "./UI/loader/TextLoader";
+import Nr from "./UI/loader/NoResult";
 function Home() {
-    const [exam, setExam] = useState([]);
-    const [loading, setLoading] = useState(true);
+  const [exam, setExam] = useState([]);
+  const [loading, setLoading] = useState(true);
 
-    const getExam = () => {
-        axios.get("get-exams").then((resp) => {
-            setExam(resp.data.data);
-            setLoading(false);
-        });
-    };
+  const getExam = () => {
+    axios.get("get-exams").then((resp) => {
+      setExam(resp.data.data);
+      setLoading(false);
+    });
+  };
 
-    useEffect(() => {
-        getExam();
-    }, []);
+  useEffect(() => {
+    getExam();
+  }, []);
 
-    const click = () => {
-        alert(1);
-    };
+  
 
-    return (
-        <>
-            <CssBaseline />
-            {!loading && <BreadCrumbs title={`EXAMS`} />}
+  return (
+    <>
+      <CssBaseline />
+      {!loading && <BreadCrumbs title={`EXAMS`} />}
 
-            <Container maxWidth="lg" style={{ padding: "0px", margin: "0px" }}>
-                <Box sx={{ bgcolor: "" }}>
-                    <Grid container spacing={1}>
-                        {(loading ? Array.from(new Array(6)) : exam).map(
-                            (examData, index) => (
-                                <Grid className="mt-4" item xs={12} md={4}>
-                                    {loading ? (
-                                        <>
-                                            <Skeleton
-                                                key={index}
-                                                variant="rectangular"
-                                                height={118}
-                                            />{" "}
-                                            <Skeleton
-                                                key={index}
-                                                variant="text"
-                                                width={"80%"}
-                                            />
-                                            <Skeleton
-                                                key={index}
-                                                variant="text"
-                                                width={"60%"}
-                                            />
-                                        </>
-                                    ) : exam.length > 0 ? (
-                                        <Card
-                                            key={examData.id}
-                                            loading={loading}
-                                            data={examData}
-                                        />
-                                    ) : (
-                                        <Nr />
-                                    )}
-                                </Grid>
-                            )
-                        )}
-                    </Grid>
-                </Box>
-            </Container>
-        </>
-    );
+      {loading ? <TextLoader /> : exam.length > 0 ? 
+       <Container maxWidth="lg" style={{ padding: "0px", margin: "0px" }}>
+       <Box sx={{ bgcolor: "" }}>
+           <Grid container spacing={1}>
+               {(loading ? Array.from(new Array(6)) : exam).map(
+                   (examData, index) => (
+                       <Grid className="mt-4" item xs={12} md={4}>
+                           {loading ? (
+                               <>
+                                   <Skeleton
+                                       key={index}
+                                       variant="rectangular"
+                                       height={118}
+                                   />{" "}
+                                   <Skeleton
+                                       key={index}
+                                       variant="text"
+                                       width={"80%"}
+                                   />
+                                   <Skeleton
+                                       key={index}
+                                       variant="text"
+                                       width={"60%"}
+                                   />
+                               </>
+                           ) : exam.length > 0 ? (
+                               <Card
+                                   key={examData.id}
+                                   loading={loading}
+                                   data={examData}
+                               />
+                           ) : (
+                               <Nr />
+                           )}
+                       </Grid>
+                   )
+               )}
+           </Grid>
+       </Box>
+   </Container>
+      
+      : <Nr />}
+    </>
+  );
 }
 
 export default Home;
