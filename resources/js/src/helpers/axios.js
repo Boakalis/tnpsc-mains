d532057298d05c2ad14def5374acfcab89238498
Boa <61935927+Boakalis@users.noreply.github.com>
2022-06-20 01:02:35 +0530

    Update axios.js

--- resources/js/src/helpers/axios.js
+++ resources/js/src/helpers/axios.js
@@ -3,7 +3,7 @@ import {useSelector} from 'react-redux';
 
 const token = JSON.parse(localStorage.getItem("rawData")) ? JSON.parse(localStorage.getItem("rawData")).token : '' ;
 export default axios.create({
-    baseURL:`http://localhost:8000/api/`,
+    baseURL:`http://tnpscmains.com/api/`,
     headers:{
         "Content-type":"application/json",
         'Authorization': 'Bearer '+token
